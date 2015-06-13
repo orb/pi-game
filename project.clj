6a2788ee156a49f33c3041e228c72e2ccb67aa7b
Norman <orb@nostacktrace.com>
2015-06-13 14:15:44 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -2,23 +2,23 @@
   :description "The PI game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.4"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [compojure "1.3.4"]
+                 [hiccup "1.0.5"]
                  [ring-edn "0.1.0"]
 
                  [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
-                 [org.clojure/clojurescript "0.0-2202"]
 
                  ;; for clojurescript
-                 [om "0.6.2"]
-                 [sablono "0.2.16"]
-                 [cljs-ajax "0.1.6"]
-                 [prismatic/dommy "0.1.1"]]
+                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.omcljs/om "0.8.8"]
 
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.7"]
-            [lein-gorilla "0.2.0"]]
+                 [sablono "0.3.4"]
+                 [cljs-ajax "0.3.12"]
+                 [prismatic/dommy "1.1.0"]]
+
+  :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-ring "0.9.5"]]
 
   :ring {:handler pi-game.core/app
          :init    pi-game.startup/on-start
@@ -30,3 +30,5 @@
                         :compiler {:output-to "public/js/app.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}]})
+
+
